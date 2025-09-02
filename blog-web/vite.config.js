03f6e299287ca2c6fe71d3499e7d17ec04f0b555
罗远祥 <1141306760@qq.com>
2025-09-02 14:21:07 +0800

    feat(infrastructure): 添加 Docker Compose 配置文件
    - 新增 docker/docker-compose.yml 文件，定义 MySQL、Redis 和博客服务的容器配置
    - 配置 MySQL 数据库、Redis 缓存和博客应用之间的依赖关系
    - 设置端口映射、数据卷和环境变量
    - 优化 Vite配置文件，调整格式和缩进

--- blog-web/vite.config.js
+++ blog-web/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue2 from '@vitejs/plugin-vue2'
 import path from 'path'
-import { loadEnv } from 'vite'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import {loadEnv} from 'vite'
+import {createSvgIconsPlugin} from 'vite-plugin-svg-icons'
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({command, mode}) => {
 
     const env = loadEnv(mode, process.cwd());
     return {
@@ -16,43 +16,43 @@ export default defineConfig(({ command, mode }) => {
             // 运行是否自动打开浏览器
             open: false,
             proxy: {
-              /** 代理前缀为 /dev-api 的请求  */
-              [env.VITE_APP_BASE_API]: {
-                changeOrigin: true,
-                // 接口地址
-                target: env.VITE_APP_API_URL,
-                rewrite: (path) =>
-                  path.replace(new RegExp("^" + env.VITE_APP_BASE_API), ""),
-              },
+                /** 代理前缀为 /dev-api 的请求  */
+                [env.VITE_APP_BASE_API]: {
+                    changeOrigin: true,
+                    // 接口地址
+                    target: env.VITE_APP_API_URL,
+                    rewrite: (path) =>
+                        path.replace(new RegExp("^" + env.VITE_APP_BASE_API), ""),
+                },
             },
         },
         plugins: [
-          vue2(),
-          createSvgIconsPlugin({
-            // 指定需要缓存的图标文件夹
-            iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-            // 指定symbolId格式
-            symbolId: 'icon-[dir]-[name]',
-          }),
+            vue2(),
+            createSvgIconsPlugin({
+                // 指定需要缓存的图标文件夹
+                iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+                // 指定symbolId格式
+                symbolId: 'icon-[dir]-[name]',
+            }),
         ],
         resolve: {
-          alias: {
-            '@': path.resolve(__dirname, './src')
-          }
+            alias: {
+                '@': path.resolve(__dirname, './src')
+            }
         },
         css: {
             preprocessorOptions: {
-              scss: {
-                api: 'modern-compiler',
-                additionalData: `
+                scss: {
+                    api: 'modern-compiler',
+                    additionalData: `
                   @import "@/styles/variables.scss";
                   @import "@/styles/mixins.scss";
                   @import "@/styles/global.scss";
                   @import "@/styles/elmentui.scss";
                 `
-              }
+                }
             }
         }
     }
-  
+
 })
